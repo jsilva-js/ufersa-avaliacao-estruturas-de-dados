34e8a40080b0fb715480597e875ec6a00a81cb1e
Jefferson Silva <contato.jefesilva@outlook.com>
2024-03-04 15:50:28 -0300

    resolve questão 10

---
+++ questao-10/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// Função A(m, n) definida recursivamente
+int A(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (m > 0 && n == 0) {
+        return A(m - 1, 1);
+    } else if (m > 0 && n > 0) {
+        return A(m - 1, A(m, n - 1));
+    }
+}
+
+// Função auxiliar para imprimir os resultados para A(x, y)
+void imprimirResultados(int a, int b) {
+    for (int x = 0; x < a; x++) {
+        for (int y = 0; y < b; y++) {
+            printf("A(%d, %d) = %d\n", x, y, A(x, y));
+        }
+    }
+}
+
+int main() {
+    int a, b;
+
+    printf("Digite o valor de a: ");
+    scanf("%d", &a);
+
+    printf("Digite o valor de b: ");
+    scanf("%d", &b);
+
+    // Chamando a função auxiliar para exibir os resultados
+    imprimirResultados(a, b);
+
+    return 0;
+}
