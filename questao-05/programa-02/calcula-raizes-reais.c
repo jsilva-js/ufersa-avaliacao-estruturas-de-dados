83555d320c14b35206b285648b8cc4de6f7ceab1
Jefferson Silva <contato.jefesilva@outlook.com>
2024-03-03 21:54:02 -0300

    resolve questão 05

---
+++ questao-05/programa-02/calcula-raizes-reais.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+void calcularRaizesReais(int a, int b, int c) {
+    int delta = b * b - 4 * a * c;
+
+    if (delta >= 0) {
+        double raiz1 = (-b + sqrt(delta)) / (2.0 * a);
+        double raiz2 = (-b - sqrt(delta)) / (2.0 * a);
+
+        printf("Raiz 1: %lf\n", raiz1);
+        printf("Raiz 2: %lf\n", raiz2);
+    } else {
+        printf("A função não possui raízes reais.\n");
+    }
+}
+
+int main() {
+    int a, b, c;
+
+    printf("Digite os coeficientes (a, b, c) da função do segundo grau: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    calcularRaizesReais(a, b, c);
+
+    return 0;
+}
