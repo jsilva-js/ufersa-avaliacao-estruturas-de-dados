b2e0c608e3e6a521cf2f61cde1a729273bec2b13
Jefferson Silva <contato.jefesilva@outlook.com>
2024-03-03 22:15:09 -0300

    resolve questão 08

---
+++ questao-08/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int inverterDigitos(int numero) {
+    int resultado = 0;
+
+    while (numero > 0) {
+        resultado = resultado * 10 + numero % 10;
+        numero /= 10;
+    }
+
+    return resultado;
+}
+
+int main() {
+    int numeroOriginal, numeroInvertido;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numeroOriginal);
+
+    // chama a função para inverter os dígitos
+    numeroInvertido = inverterDigitos(numeroOriginal);
+
+    printf("Numero inserido: %d\n", numeroOriginal);
+    printf("Numero invertido: %d\n", numeroInvertido);
+
+    return 0;
+}
