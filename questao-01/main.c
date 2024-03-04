a357ccb84e35632fbf4890ac9da4c16dcf819d92
Jefferson Silva <contato.jefesilva@outlook.com>
2024-03-04 16:05:37 -0300

    revisa código da questão 01 e executa testes com gdb debugger

--- questao-01/main.c
+++ questao-01/main.c
@@ -3,20 +3,16 @@
 int main() {
     int numero;
 
-    // Solicita e ler cinco números
     for (int i = 0; i < 5; i++) {
         printf("Digite um número entre 1 e 30: ");
         scanf("%d", &numero);
 
-        // Verificar se o número está dentro do intervalo entre 1 e 30
         if (numero >= 1 && numero <= 30) {
-            // Imprime asteriscos de acordo com o número fornecido
             for (int j = 0; j < numero; j++) {
                 printf("*");
             }
             printf("\n");
         } else {
-            // Caso o número não esteja no intervalo permitido
             printf("Número inválido! Digite um número entre 1 e 30.\n");
             // Decrementa o contador para pedir o mesmo número novamente
             i--;
